aab0bc31b0c5dad07cdcf80e41cfa75aa0136ab2
Micheal Ngene <mngene4@gmail.com>
2025-09-30 10:25:19 +0200

    Modified for local dev

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -2,8 +2,8 @@ const Book = require("../models/book.model.js");
 const { body, validationResult } = require("express-validator");
 
 exports.validateBook = [
-    body("title").notEmpty().withMessage("Title is required"),
-    body("author").notEmpty().withMessage("Author is required"),
+    body("title").notEmpty().withMessage("Title is required!"),
+    body("author").notEmpty().withMessage("Author is required!"),
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
